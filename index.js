86eb88ad2c00c680f7b0f33ee89ac71ac6330430
Brian <mahuibrian14@gmail.com>
2023-04-11 15:29:46 +0300

    complete

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    /*
+    Check if a string is a palindrome.
+    
+    Argument:
+        word(string): The input string.
+    
+    Returns:
+        boolean: true if the string is a palindrome, false if not.
+    */
+    // Convert the input string to lowercase and remove spaces
+     = word.toLowerCase().replace(/\s+/g, '');
+    
+    // Loop through the string and compare each character
+    // from the beginning and the end of the string
+    for (let i = 0; i < Math.floor(word.length / 2); i++) {
+        if (word[i] !== word[word.length - 1 - i]) {
+            return false;
+        }
+    }
+    
+    return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
